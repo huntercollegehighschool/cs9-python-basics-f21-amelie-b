dcabac066da1df6951f29736a78de0edf4fa64e1
Amelie Bi <94643781+amelie-b@users.noreply.github.com>
2021-12-04 14:31:02 +0000

    I added more values and inserted more of those values into the sum

--- part3.py
+++ part3.py
@@ -7,8 +7,8 @@ There are (at least) 6 errors in this code. Fix them so that it runs properly.
 '''
 
 #code starts here
-number1 = input("Enter a number: ")
-number2 = int(input("Enter another number: ")
+number_one = int(input("Enter a number: "))
+number_two = int(input("Enter another number: "))
 
-print "The sum of your numbers is", number1 + Number2
-print(Seven times your second number is, 7(number2))
+print("The sum of your numbers is", number_one + number_two)
+print("Seven times your second number is", 7 * number_two)
